4ee8f0a706a16fa0d0b50fd09c112f4b6c6f35f1
Oliver Ochs <oliver.ochs@holisticon.de>
2018-02-06 22:34:24 +0100

    renamed stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,42 @@
  * PIR sensor tester
  */
  
-int pir1pin = 2;               // choose the input pin (for PIR sensor)
-int pir1State = LOW;             // we start, assuming no motion detected
-int pir2pin = 3;
-int pir2State = LOW;
-int pir3pin = 4;
-int pir3State = LOW;
-int val1 = 0;                    // variable for reading the pin status
-int val2 = 0;
-int val3 = 0;
+int leftPirPin = 2;               // choose the input pin (for PIR sensor)
+int leftPirState = LOW;             // we start, assuming no motion detected
+int rightPirPin = 3;
+int rightPirState = LOW;
+int centerPirPin = 4;
+int centerPirState = LOW;
+int leftValue = 0;                    // variable for reading the pin status
+int rightValue = 0;
+int centerValue = 0;
+int ledPin = 13;  
 Servo myservo;
 
 void setup() {
-  pinMode(pir1pin, INPUT);     // declare sensor as input
-  pinMode(pir2pin, INPUT);
-  pinMode(pir3pin, INPUT);
+  pinMode(leftPirPin, INPUT);     // declare sensor as input
+  pinMode(rightPirPin, INPUT);
+  pinMode(centerPirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
   myservo.attach(5, 795, 2180); 
   Serial.begin(9600);
 }
  
 void loop(){
-  val1 = digitalRead(pir1pin);  // read input value
-  val2 = digitalRead(pir2pin); // read second sensor
-  val3 = digitalRead(pir3pin);
-  Serial.print(val1);
-  Serial.print(val2);
-  Serial.println(val3);
+  leftValue = digitalRead(leftPirPin);  // read input value
+  rightValue = digitalRead(rightPirPin); // read second sensor
+  centerValue = digitalRead(centerPirPin);
+  Serial.print(leftValue);
+  Serial.print(rightValue);
+  Serial.println(centerValue);
 
-  if (val1 == HIGH) {            // check if the input is HIGH
+  if (leftValue == HIGH) {            // check if,the input is HIGH
     myservo.write(1);
+    digitalWrite(ledPin, LOW);
+
   } else {
     myservo.write(90);
+    digitalWrite(ledPin, HIGH);
+
   }
 }
